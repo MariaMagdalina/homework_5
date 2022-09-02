68c35796ee83e69a23ca11f1a0211c3c3a56885e
MariaMagdalina <metodist_rmmc@mail.ru>
2022-09-02 17:40:25 +0300

    задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// программа находит сумму четных элементов одномерного массива, стоящих на нечётных позициях 
+Console.Write("Введите размер массива ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N]; 
+int sum = 0;
+for(int i=0; i<N; i++)
+ {
+   array[i] = new Random().Next(0,1000000);
+   Console.WriteLine(array[i]);
+   if((array[i] % 2 == 0) && (i % 2) != 0)
+      sum = sum + array[i];
+ }
+Console.WriteLine("сумма четных элементов массива, стоящих на нечётных позициях, равна " + sum);
